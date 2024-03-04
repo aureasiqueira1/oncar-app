298c96607eb7500189087984fea05cb808ce0102
aureasiqueira1 <aurea@gec.inatel.br>
2024-03-04 09:49:18 -0300

    config cors

--- api-oncarapp/src/main.ts
+++ api-oncarapp/src/main.ts
@@ -2,13 +2,11 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { config } from 'dotenv';
-import { CustomSocketIoAdapter } from './custom-socket-io-adapter';
 
 async function bootstrap() {
   config();
   const app = await NestFactory.create(AppModule);
 
-  app.useWebSocketAdapter(new CustomSocketIoAdapter(app));
 
   app.enableCors(); // Habilita CORS globalmente
 
