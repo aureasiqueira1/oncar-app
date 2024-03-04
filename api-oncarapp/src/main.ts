1889d13d3a5025da9e0ce982226f7a57e8e1ee79
aureasiqueira1 <aurea@gec.inatel.br>
2024-03-04 12:07:19 -0300

    add links

--- api-oncarapp/src/main.ts
+++ api-oncarapp/src/main.ts
@@ -9,18 +9,20 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
 
-
-  const options = {
-    origin: "*",
-    methods: "GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS",
-    preflightContinue: false,
-    optionsSuccessStatus: 204,
-    credentials: true
-  };
-
-  app.enableCors(options);
-
-
+  app.enableCors({
+    origin: [
+      'http://localhost:3000',
+      'http://localhost:3001',
+      'http://localhost:3001/car',
+      'http://localhost:3001/user',
+      'https://front-oncar.vercel.app',
+      'https://api-oncar.vercel.app',
+      'https://api-oncar.vercel.app/car',
+      'https://api-oncar.vercel.app/user',
+    ],
+    methods: ["GET", "POST", "DELETE", "PATCH", "PUT"],
+    credentials: true,
+  });
 
   await app.listen(3001);
 }
