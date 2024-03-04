604a9fec396f9cb836efd8fadca8da77d354900f
aureasiqueira1 <aurea@gec.inatel.br>
2024-03-04 10:17:45 -0300

    config cors

--- api-oncarapp/src/main.ts
+++ api-oncarapp/src/main.ts
@@ -2,24 +2,17 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { config } from 'dotenv';
+import cors from 'cors';
 
 async function bootstrap() {
   config();
   const app = await NestFactory.create(AppModule);
 
-  app.use(function(req, res, next) {
-    res.header("Access-Control-Allow-Origin", "https://front-oncar-8sgt9tqfp-aureasiqueira1s-projects.vercel.app");
-    res.header("Access-Control-Allow-Origin", "https://front-oncar.vercel.app"); // Update to match the domain you will make the request from
-    res.header("Access-Control-Allow-Origin", "https://front-oncar.vercel.app/car"); // Update to match the domain you will make the request from
-    res.header("Access-Control-Allow-Origin", "https://front-oncar.vercel.app/user"); // Update to match the domain you will make the request from
-    res.header("Access-Control-Allow-Origin", "https://api-oncar.vercel.app"); // Update to match the domain you will make the request from
-    res.header("Access-Control-Allow-Origin", "https://api-oncar.vercel.app/car"); // Update to match the domain you will make the request from
-    res.header("Access-Control-Allow-Origin", "https://api-oncar.vercel.app/user"); // Update to match the domain you will make the request from
 
-    res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-    next();
-  });
+  app.use(cors({
+   origin: 'https://front-oncar.vercel.app'
+  }));
+  
 
 
   https://api-oncar.vercel.app/user
