db5aefc941105845f14b98b97b5025b6dda8dceb
aureasiqueira1 <aurea@gec.inatel.br>
2024-03-04 11:25:07 -0300

    add enableCors()

--- api-oncarapp/src/main.ts
+++ api-oncarapp/src/main.ts
@@ -1,24 +1,12 @@
 /* eslint-disable prettier/prettier */
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import cors from 'cors';
 import { config } from 'dotenv';
 
 async function bootstrap() {
   config();
 
   const app = await NestFactory.create(AppModule);
-
-  const corsOptions = {
-    origin: true,
-    methods: 'GET,PUT,POST,DELETE',
-    allowedHeaders: 'Content-Type, Accept',
-    credentials: true,
-    optionsSuccessStatus: 200,
-    accessControlAllowHeaders: 'Accept',
-  };
-
-  app.use(cors(corsOptions));
 
   app.enableCors();
 
