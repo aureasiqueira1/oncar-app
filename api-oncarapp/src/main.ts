5ed98ff3b154c59099f7558999ff568cd9b7b93a
aureasiqueira1 <aurea@gec.inatel.br>
2024-03-04 10:00:51 -0300

    config cors

--- api-oncarapp/src/main.ts
+++ api-oncarapp/src/main.ts
@@ -7,7 +7,14 @@ async function bootstrap() {
   config();
   const app = await NestFactory.create(AppModule);
 
+  app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "https://front-oncar-8sgt9tqfp-aureasiqueira1s-projects.vercel.app");
+    res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+  });
 
+  
   app.enableCors(); // Habilita CORS globalmente
 
   await app.listen(3001);
