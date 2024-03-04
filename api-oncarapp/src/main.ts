38a28182997b794ce19f32b33c7f4a48dd9e665f
aureasiqueira1 <aurea@gec.inatel.br>
2024-03-04 11:28:54 -0300

    app use

--- api-oncarapp/src/main.ts
+++ api-oncarapp/src/main.ts
@@ -2,13 +2,23 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { config } from 'dotenv';
+import cors from 'cors';
 
 async function bootstrap() {
   config();
 
   const app = await NestFactory.create(AppModule);
 
-  app.enableCors();
+  const corsOptions = {
+    origin: "*",
+    methods: 'GET,PUT,POST,DELETE',
+    allowedHeaders: 'Content-Type, Accept',
+    credentials: true,
+    optionsSuccessStatus: 200,
+    accessControlAllowHeaders: 'Accept',
+  };
+
+  app.use(cors(corsOptions));
 
   await app.listen(3001);
 }
