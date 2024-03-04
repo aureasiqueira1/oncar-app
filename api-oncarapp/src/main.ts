f88c41157812922c6d1a9646bc5c5752f11c3d93
aureasiqueira1 <aurea@gec.inatel.br>
2024-03-04 12:31:52 -0300

    configs nestjs

--- api-oncarapp/src/main.ts
+++ api-oncarapp/src/main.ts
@@ -7,19 +7,9 @@ async function bootstrap() {
   config();
 
   const app = await NestFactory.create(AppModule);
-  const whitelist = ['http://localhost:3001/', 'http://localhost:3001/car', 'http://localhost:3001/user', 'http://localhost:3000/', 'http://localhost:3000/car', 'http://localhost:3000/user', 
-  'https://front-oncar.vercel.app/', 'https://front-oncar.vercel.app/car', 
-  'https://front-oncar.vercel.app/user', 'https://api-oncar.vercel.app/', 
-  'https://api-oncar.vercel.app/car', 'https://api-oncar.vercel.app/user'];
 
   app.enableCors({
-    origin: function (origin, callback) {
-       if (whitelist.indexOf(origin) !== -1) {
-         callback(null, true);
-       } else {
-         callback(new Error('Not allowed by CORS'));
-       }
-    },
+    origin: 'https://api-oncar.vercel.app',
     credentials: true,
    });
 
