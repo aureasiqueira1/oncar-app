995ba1f90de0a04abe6d14a8a4be3ea709999b67
aureasiqueira1 <aurea@gec.inatel.br>
2024-03-04 12:39:34 -0300

    removendo /

--- api-oncarapp/src/main.ts
+++ api-oncarapp/src/main.ts
@@ -9,7 +9,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.enableCors({
-    origin: 'https://api-oncar.vercel.app/',
+    origin: ['https://api-oncar.vercel.app', 'https://api-oncar.vercel.app/car', 'https://api-oncar.vercel.app/user'],
     credentials: true,
    });
 
