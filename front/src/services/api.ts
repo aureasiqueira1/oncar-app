9010310cad3c4ee6d9ff1386c42083c8b779b24a
aureasiqueira1 <aurea@gec.inatel.br>
2024-03-04 09:55:16 -0300

    config cors

--- front/src/services/api.ts
+++ front/src/services/api.ts
@@ -2,5 +2,5 @@ import axios from "axios";
 
 export const api = axios.create({
   // baseURL: "http://localhost:3001",
-  baseURL: "https://front-oncar.vercel.app",
+  baseURL: "https://api-oncar.vercel.app",
 });
