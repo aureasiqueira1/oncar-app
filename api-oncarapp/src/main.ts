f1cf35b38160eb1040420109dd48cb06c5f877e0
aureasiqueira1 <aurea@gec.inatel.br>
2024-03-04 09:24:35 -0300

    instalando cors

--- api-oncarapp/src/main.ts
+++ api-oncarapp/src/main.ts
@@ -1,3 +1,4 @@
+/* eslint-disable prettier/prettier */
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { config } from 'dotenv';
@@ -6,11 +7,7 @@ async function bootstrap() {
   config();
   const app = await NestFactory.create(AppModule);
 
-  app.enableCors({
-    origin: 'https://front-oncar.vercel.app/', // Restrict to your frontend domain
-    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
-    allowedHeaders: 'Content-Type,Authorization',
-  });
+  app.enableCors(); // Habilita CORS globalmente
 
   await app.listen(3001);
 }
