07c9b874e1be07d50563d5cda20df6470fff077b
aureasiqueira1 <aurea@gec.inatel.br>
2024-03-04 10:07:28 -0300

    config cors

--- api-oncarapp/src/main.ts
+++ api-oncarapp/src/main.ts
@@ -10,6 +10,8 @@ async function bootstrap() {
   app.use(function(req, res, next) {
     res.header("Access-Control-Allow-Origin", "https://front-oncar-8sgt9tqfp-aureasiqueira1s-projects.vercel.app");
     res.header("Access-Control-Allow-Origin", "https://front-oncar.vercel.app"); // Update to match the domain you will make the request from
+    res.header("Access-Control-Allow-Origin", "https://front-oncar.vercel.app/car"); // Update to match the domain you will make the request from
+    res.header("Access-Control-Allow-Origin", "https://front-oncar.vercel.app/user"); // Update to match the domain you will make the request from
     res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
     next();
