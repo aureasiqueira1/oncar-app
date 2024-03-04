e3e259284cf0f4046bd990872c8db1be2158eb29
aureasiqueira1 <aurea@gec.inatel.br>
2024-03-04 09:41:09 -0300

    config cors

--- api-oncarapp/src/custom-socket-io-adapter/index.ts
+++ api-oncarapp/src/custom-socket-io-adapter/index.ts
@@ -5,7 +5,7 @@ export class CustomSocketIoAdapter extends IoAdapter {
   createIOServer(port: number, options?: socketio.ServerOptions): any {
     const server = super.createIOServer(port, options);
 
-    server.origins('http://localhost:3000', 'https://api-oncar.vercel.app'); // Defina as origens aqui
+    server.origins('http://localhost:3000', 'https://api-oncar.vercel.app/'); // Defina as origens aqui
 
     return server;
   }
