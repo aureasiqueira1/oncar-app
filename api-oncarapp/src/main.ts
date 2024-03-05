530eb537a0772a176e5092f3c91c039cb1412033
aureasiqueira1 <aurea@gec.inatel.br>
2024-03-05 08:24:16 -0300

    configuração dos links de origem no main

--- api-oncarapp/src/main.ts
+++ api-oncarapp/src/main.ts
@@ -9,17 +9,22 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.enableCors({
-    origin: ['https://front-oncar.vercel.app', 
-    'https://front-oncar.vercel.app/car', 
-    'https://front-oncar.vercel.app/user', 
-    'https://api-oncar.vercel.app', 
-    'https://api-oncar.vercel.app/car', 
-    'https://api-oncar.vercel.app/user'],
-    credentials: true,
+    origin: [
+      'http://localhost:3000/',
+      'http://localhost:3001/user',
+      'http://localhost:3000',
+      'http://localhost:3001/car',
 
- 
-    
-   });
+      'https://front-oncar.vercel.app/',
+      'https://api-oncar.vercel.app/user',
+      'https://front-oncar.vercel.app',
+      'https://api-oncar.vercel.app/car'
+    ],
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    // headers exposed to the client
+    credentials: true, 
+
+  });
 
   await app.listen(3001);
 }
